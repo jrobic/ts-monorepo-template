34238a13c34dcb484efc017fef27f62f5979af1e
Jonathan Robic <hello@jonathanrobic.fr>
2023-04-20 11:20:55 +0200

    feat: awesome breaking changes :rocket:
    BREAKING CHANGE: awesome

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR!
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
